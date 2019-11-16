bf17c422f0958c13e542d055b6983f50d9fa9939
Shubham Gupta <shubham@deligence.com>
2019-11-16 22:30:40 +0530

    Updates

---
+++ precache-manifest.6f0fa536f74f56367799c4988d74e938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14169e19108f991de94c8197757a7818",
+    "url": "/github.io/monsters/index.html"
+  },
+  {
+    "revision": "73285978d444c1dcf61e",
+    "url": "/github.io/monsters/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "0ddff37250ede6692503",
+    "url": "/github.io/monsters/static/css/main.8ccfe367.chunk.css"
+  },
+  {
+    "revision": "73285978d444c1dcf61e",
+    "url": "/github.io/monsters/static/js/2.027105a0.chunk.js"
+  },
+  {
+    "revision": "0ddff37250ede6692503",
+    "url": "/github.io/monsters/static/js/main.34ebadb8.chunk.js"
+  },
+  {
+    "revision": "b9c2b7ad49aaec7f8c51",
+    "url": "/github.io/monsters/static/js/runtime-main.c56de428.js"
+  }
+]);
