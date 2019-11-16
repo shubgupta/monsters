77a121fc66bf874cd555a9efe3daa0d55bc996fc
Shubham Gupta <shubham@deligence.com>
2019-11-16 22:27:18 +0530

    Deploy changes

--- src/component/card/card.jsx
+++ src/component/card/card.jsx
@@ -3,7 +3,7 @@ import './card.css'
 export const Card = props=>(
   <div className=" p-0 col-sm-3 monsterCard">
     <img src={`https://robohash.org/${props.monster.id}?set=set2&size=100x100`} alt=""/>
-    <h1>{props.monster.name}</h1>
+    <h2>{props.monster.name}</h2>
     <p>{props.monster.email}</p>
   </div>
 )
--- src/component/cardlist/cardlist.jsx
+++ src/component/cardlist/cardlist.jsx
@@ -3,9 +3,12 @@ import { Card } from '../card/card.jsx'
 export const Cardlist = props => (
   <div className="row p-0 m-0">
     {
-      props.monsters.map(monster => (
-        <Card key={monster.id} monster={monster} />
-      ))
+      props.monsters.length ?
+        props.monsters.map(monster => (
+          <Card key={monster.id} monster={monster} />
+        ))
+        :
+        <h1 className="text-center text-white mt-5 col-12">No Monster Found</h1>
     }
   </div>
 );
